a4f3d72ac4a8d010c8b9db2a27f1a6eb4a5b976d
Marina Zonova <114925418+marinazonz@users.noreply.github.com>
2023-04-08 19:34:56 +0200

    Finishing

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,7 +1,13 @@
 import "./article.css";
 
 const Article = () => {
-    return <div>Article</div>;
+    return (
+        <div className='gpt3__blog-container_article'>
+            <div className='gpt3__blog-container_article-image'>
+                <img src={imgUrl} alt='article' />
+            </div>
+        </div>
+    );
 };
 
 export default Article;
